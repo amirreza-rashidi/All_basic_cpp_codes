03ac1734f0474ef50c6c3c0b2d2a9c2b75613880
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2025-07-13 00:11:18 +0330

    while3

---
+++ While3.cpp
@@ -0,0 +1,18 @@
+// Calculate the sum of the integers from 1 to 10 
+#include <iostream>
+using namespace std;
+
+int main() {
+   unsigned int sum{0};
+   unsigned int x{1};
+
+   while (x <= 10) { // while x is less than or equal to 10
+      sum += x; // add x to sum
+      ++x; // increment x
+   } 
+
+   cout << "The sum is: " << sum << endl;
+} 
+
+
+
